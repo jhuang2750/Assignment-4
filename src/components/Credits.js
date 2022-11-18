d191c472b124909f2d96643d291880ce7ef66071
Jack Huang <jhuang2750@gmail.com>
2022-11-18 15:52:25 -0500

    Added credits.

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -7,10 +7,31 @@ Note: You need to work on this file for the Assignment.
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
+  let creditsView = () => {
+    const {credits} = props;
+    return credits.map((credit) => {
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    })
+  }
+
+const newCredit = (event) =>{
+  let newDate = new Date().toISOString();
+  let newAmount = event.target.amount.value;
+  let newDescr = event.target.description.value;
+  let newID = newDate;
+  this.props.addCredit(newID, newAmount, newDescr, newDate);
+}
   return (
     <div>
       <h1>Credits</h1>
+      {creditsView()}
       <br/>
+      <form onSubmit={newCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">AddCredit</button>
+      </form>
       <Link to="/">Return to Home</Link>
     </div>
   )
